36dcbef4645b311d9646607ec8e9be0975aa83b0
Vivek_Ionetic <nandan@test.com>
2026-09-10 15:25:38 +0530

    Resolve refrence issue

--- src/BuildingBlocks/BuildingBlocks.Observability/SerilogBootstrap.cs
+++ src/BuildingBlocks/BuildingBlocks.Observability/SerilogBootstrap.cs
@@ -1,11 +1,8 @@
+using BuildingBlocks.Observability.Middleware;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
 using Serilog.Formatting.Compact;
-using BuildingBlocks.Observability.Middleware;
 
 namespace BuildingBlocks.Observability;
 
--- src/Gateway/ApiGateway/Program.cs
+++ src/Gateway/ApiGateway/Program.cs
@@ -1,8 +1,7 @@
-using System.Threading.RateLimiting;
 using BuildingBlocks.Observability;
 using BuildingBlocks.Observability.Middleware;
-using Microsoft.AspNetCore.RateLimiting;
 using Serilog;
+using System.Threading.RateLimiting;
 
 const string ServiceName = "ApiGateway";
 
